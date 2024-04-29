0208804f47a2878da0094063b33a521b67c86927
Ramin Mohammadi <26876927+raminmohammadi@users.noreply.github.com>
2024-04-29 07:51:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     
     name = 'AUTOGRAD',
-    version = '0.1.0',
+    version = '1.0.0',
     packages = find_packages(),
     install_requires = [
         'numpy>=1.18.0',
