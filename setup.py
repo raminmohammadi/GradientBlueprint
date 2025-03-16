44c3eca23ed90ffa850c75acf6ea84c89256d8e0
raminmohammadi <ramin.mohammadi@tausight.com>
2025-03-16 16:36:21 -0400

    fixed a typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     
     name = 'gradientblueprint',
-    version = '1.0.0',
+    version = '1.0.1',
     packages = find_packages(),
     install_requires = [
         'numpy>=1.18.0',
@@ -18,7 +18,7 @@ setup(
     description='This is a comprehensive package designed to demystify the concepts of gradients and their applications across various machine learning algorithms. With this release, developers and researchers gain access to a powerful toolkit that simplifies the understanding and implementation of gradient-based optimization techniques.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/raminmohammadi/GredientBlueprint',
+    url='https://github.com/raminmohammadi/GradientBlueprint',
     classifiers=[
         'Programming Language :: Python :: 3',
         "License :: OSI Approved :: MIT License",
